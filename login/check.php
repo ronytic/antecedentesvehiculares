dd7d77c82a2f08ffbc0f72e140677055eceb5eef
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2019-05-12 09:19:30 -0400

    Login

---
+++ login/check.php
@@ -0,0 +1,20 @@
+<?php
+session_start();
+$dir=dirname(__FILE__).DIRECTORY_SEPARATOR."../";
+define("RAIZ",$dir);
+include_once(RAIZ."configuracion.php");
+//include_once(RAIZ."rastreo/revisar.php");
+if(!(isset($_SESSION["LoginSistemaAntecedentes"]) && $_SESSION['LoginSistemaAntecedentes']==1)){
+	include_once(RAIZ."funciones/url.php");
+	header("Location:".url_base().$directory."login/?u=".$_SERVER['PHP_SELF']);
+}else{
+	include_once(RAIZ."funciones/url.php");
+	include_once(RAIZ."funciones/funciones.php");
+	include_once(RAIZ."class/menu.php");
+	$menu=new menu();
+	$s_url= separar_url($directory,url_sub());
+	$res_menu=$menu->obtenerOpcion($s_url[0]);
+	setcookie("Opcion","asd",time()-1);
+    setcookie("Opcion",$res_menu['CodMenu']);
+}
+?>
