6b4309feb25e6e91afec8054391140d36d8e2871
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2019-05-12 09:19:37 -0400

    Create pie.php

---
+++ pie.php
@@ -0,0 +1,35 @@
+</div>
+            <div class="footer ">
+                <div class="row">
+                    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 text-left">
+                        <strong>Todos los Derechos Reservados  &copy; <?=date("Y")!="2019"?'2019':'';?> <?php echo date("Y")?></strong>
+                    </div>
+                    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 text-right" title="Cel: (591)-73230568">
+                        Sistema Desarrollado por: <a href="http://fb.com/ronaldnina" target="_blank" title="Cel: (591)-73230568"><strong>Ronald Nina Layme</strong></a>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        </div>
+    <script language="javascript">
+        var folder="<?php echo $folder?>";
+    </script>
+    <!-- Principal scripts -->
+
+    <script src="<?php echo $folder?>js/core/bootstrap.min.js"></script>
+    <script src="<?php echo $folder?>js/plugins/cookie/js.cookie.js"></script>
+    <script src="<?php echo $folder?>js/plugins/select2-3.5.2/select2.min.js"></script>
+    <script src="<?php echo $folder?>js/plugins/metisMenu/jquery.metisMenu.js"></script>
+    <script src="<?php echo $folder?>js/plugins/slimscroll/jquery.slimscroll.min.js"></script>
+
+    <!-- Plugins Modificados Javascript -->
+    <script src="<?php echo $folder?>js/core/inspinia.js"></script>
+    <script src="<?php echo $folder?>js/plugins/form/jquery.form.js"></script>
+    <script src="<?php echo $folder?>js/plugins/busqueda/busqueda.js"></script>
+    <!-- <script src="<?php echo $folder?>js/plugins/pace/pace.min.js"></script>-->
+
+
+</body>
+
+</html>
